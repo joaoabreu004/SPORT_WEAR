40e22eab7bbcb53efa7071b09be37cc75b4cc19a
João Abreu <jvabreusousa12@gmail.com>
2022-12-18 22:30:37 -0300

    styles: melhorando UI

--- src/layout/Propaganda/index.js
+++ src/layout/Propaganda/index.js
@@ -1,21 +1,25 @@
 import styles from './Propaganda.module.scss'
 import imgPropaganda from '../../img/img_propaganda.png'
+import {FaArrowRight} from "react-icons/fa"
 
 function Propaganda() {
     return (
         <div className={styles.propadanda__section}>
+            <div className={styles.propadanda__section__textos}>
+                <p>
+                    CAMISAS COPA DO MUNDO 2022
+                    <br/>
+                    CHAMPIONS LEAGUE 2022/23
+                    <br/>
 
-            <div className={styles.propadanda__title}>
-                FINAL DE ANO, SUPRENDA COM O ESPORTE!
-               
+                    E MUITO MAIS AQUI NA SPORT WEAR!
+                </p>
+                <p>SURPEENDA COM O MELHOR DO ESPORTE</p>
+                <button>VER MAIS <FaArrowRight/> </button>
             </div>
 
-            <button className={styles.propaganda__button}>
-                    VER MAIS
-            </button>
-
-            <div className={styles.div__img}>
-                <img className={styles.propadanda__img} src={imgPropaganda} alt="PROPAGANDA"/>
+            <div className={styles.propadanda__section__imagem}>
+                <img src={imgPropaganda} alt="Imagem Propaganda" className={styles.img}/>
             </div>
 
         </div>
